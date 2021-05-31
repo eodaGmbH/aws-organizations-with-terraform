b4cae89b7ac66bc72114e0c14f9df2f2c3b40edd
Soeren Wegener <soeren.wegener@eoda.de>
2021-05-31 15:52:44 +0200

    Step 1: Add an AWS organizations account

---
+++ modules/aws-account/main.tf
@@ -0,0 +1,17 @@
+resource "aws_organizations_account" "account" {
+  # Account name for the "My organizations" overview
+  # This is not the account alias which can be used
+  # when switching roles
+  name = var.account_name
+  # Globally unique email address.
+  # Each email account can only have one AWS account
+  email = var.email_address
+  # A role will be automatically created which has
+  # full admin rights. Let's call it "owner"
+  role_name = "owner"
+  # Tags are not required, but might be helpful for others
+  # in the future who read the assigned tags.
+  tags = {
+    "terraform-managed" : true
+  }
+}
